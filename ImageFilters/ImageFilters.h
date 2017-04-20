98b964c30cca1001d2171539b521f5c158c8afe4
Siddhesh Mhatre <Sid@Siddheshs-MacBook-Pro.local>
2017-04-20 17:35:06 +0530

    first commit

---
+++ ImageFilters/ImageFilters.h
@@ -0,0 +1,13 @@
+//
+//  ImageFilters.h
+//  ImageFilters
+//
+//  Created by Siddhesh Mhatre on 20/04/17.
+//  Copyright © 2017 Siddhesh Mhatre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageFilters : NSObject
+
+@end
---
+++ ImageFilters/ImageFilters.m
@@ -0,0 +1,13 @@
+//
+//  ImageFilters.m
+//  ImageFilters
+//
+//  Created by Siddhesh Mhatre on 20/04/17.
+//  Copyright © 2017 Siddhesh Mhatre. All rights reserved.
+//
+
+#import "ImageFilters.h"
+
+@implementation ImageFilters
+
+@end
